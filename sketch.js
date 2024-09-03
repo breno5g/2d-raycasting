70fd09c792dcf6ccfcf6954ab7b9e7cb783bf845
Breno Santos <brenosantos145@gmail.com>
2024-09-03 14:56:07 -0300

    feat: improve responsivity

--- sketch.js
+++ sketch.js
@@ -4,15 +4,31 @@ let particle;
 let xoff = 0;
 let yoff = 10000;
 
+function initCanvas() {
+  let screenWidth = window.innerWidth;
+  let screenHeight = window.innerHeight;
+
+  if (screenWidth > 800) {
+    screenWidth = 800;
+  }
+  if (screenHeight > 800) {
+    screenHeight = 800;
+  }
+
+  createCanvas(screenWidth - 50, screenHeight - 50);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  initCanvas();
+  // createCanvas(400, 400);
   // boundary = new Boundary(100, 100, 300, 300);
   particle = new Particle();
-  walls = Boundary.createRandom(5);
+  walls = Boundary.createRandom(10);
 }
 
 function draw() {
   background(0);
+
   walls.forEach((wall) => {
     wall.show();
   });
