e8b8f0a3e79d0b3abd6cf10199db9098117b5351
Breno Santos <brenosantos145@gmail.com>
2024-09-03 13:31:02 -0300

    feat: adding perling noise to move

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 let walls = [];
 let particle;
+
+let xoff = 0;
+let yoff = 10000;
+
 function setup() {
   createCanvas(400, 400);
   // boundary = new Boundary(100, 100, 300, 300);
@@ -14,6 +18,10 @@ function draw() {
   });
 
   particle.show();
-  particle.updatePosition(mouseX, mouseY);
+  // particle.updatePosition(mouseX, mouseY);
+  particle.updatePosition(noise(xoff) * width, noise(yoff) * height);
   particle.cast(walls);
+
+  xoff += 0.01;
+  yoff += 0.01;
 }
